6d6bc892428171e74f9c3b7acd1ff8366b5a07ac
David Eilers <david.eilers@s2017.tu-chemnitz.de>
2022-01-28 18:23:09 +0100

    Refactored glfw.hpp

--- src/glfw.hpp
+++ src/glfw.hpp
@@ -19,34 +19,35 @@ void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum se
                 << ", message = " << message;
 }
 
+struct Application{
+    virtual void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)=0;
+    virtual void setup()=0;
+    virtual void draw(int width, int height)=0;
+};
+
 class ContextManager
 {
   public:
-    int f = 5;
-
-    struct Vertex
-    {
-        float x, y;
-        float r, g, b;
-    };
-    Vertex vertices[3] = {{-0.6f, -0.4f, 1.f, 0.f, 0.f}, {0.6f, -0.4f, 0.f, 1.f, 0.f}, {0.f, 0.6f, 0.f, 0.f, 1.f}};
+   
 
     static void error_callback(int error, const char *description)
     {
         logger_error << description;
     }
 
-    static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+    void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
     {
         if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
             glfwSetWindowShouldClose(window, GLFW_TRUE);
+        m_app->key_callback( window, key, scancode, action, mods);
     }
 
-    ContextManager()
+    Application* m_app;
+
+    ContextManager(Application *app_): m_app{app_}
     {
         GLFWwindow *window;
-        GLuint vertex_buffer, vertex_shader, fragment_shader, program, vertex_array_object;
-        GLint mvp_location, vpos_location, vcol_location;
+
 
         glfwSetErrorCallback(error_callback);
 
@@ -62,10 +63,10 @@ class ContextManager
         {
             logger_error << "Failed to create Window or OpenGL Context!";
             glfwTerminate();
-            exit(EXIT_FAILURE);
+            return;
         }
 
-        glfwSetKeyCallback(window, key_callback);
+        //glfwSetKeyCallback(window, key_callback);
 
         glfwMakeContextCurrent(window);
         gladLoadGL(glfwGetProcAddress);
@@ -73,75 +74,14 @@ class ContextManager
         logger_info << "OpenGL Version: " << glGetString(GL_VERSION);
         glfwSwapInterval(1);
 
-        #ifdef DEBUG_GL
-        glEnable(GL_DEBUG_OUTPUT);
-        glDebugMessageCallback(MessageCallback, 0);
-        #endif
-
-        // NOTE: OpenGL error checks have been omitted for brevity
-
-        glGenVertexArrays(1, &vertex_array_object);
-        glBindVertexArray(vertex_array_object);
-        glGenBuffers(1, &vertex_buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        /*
-           vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-           glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-           glCompileShader(vertex_shader);
-
-           fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-           glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-           glCompileShader(fragment_shader);
-
-           program = glCreateProgram();
-           glAttachShader(program, vertex_shader);
-           glAttachShader(program, fragment_shader);
-           glLinkProgram(program);
-          */
-        std::string shader_dir("assets\\shader\\");
-        std::string vertex_shader_path(shader_dir + "example.vert.glsl");
-        std::string fragment_shader_path(shader_dir + "example.frag.glsl");
-        Shader myShader{vertex_shader_path, fragment_shader_path};
-        vertex_shader = myShader.m_vertex_shader_id;
-        fragment_shader = myShader.m_fragment_shader_id;
-        program = myShader.m_program_id;
-
-        mvp_location = glGetUniformLocation(program, "MVP");
-        vpos_location = glGetAttribLocation(program, "vPos");
-        vcol_location = glGetAttribLocation(program, "vCol");
-
-        if (mvp_location == -1 || vpos_location == -1 || vcol_location == -1)
-        {
-            logger_warn << "Some uniform location was not found!";
-        }
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void *)0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void *)(sizeof(float) * 2));
-
-        glClearColor(0.5f, 0.5f, 0.5, 1.0);
+        m_app->setup();
 
         while (!glfwWindowShouldClose(window))
         {
-            float ratio;
             int width, height;
-            mat4x4 m, p, mvp;
 
             glfwGetFramebufferSize(window, &width, &height);
-            ratio = width / (float)height;
-
-            glViewport(0, 0, width, height);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            mat4x4_identity(m);
-            mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-            mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-            mat4x4_mul(mvp, p, m);
-
-            glUseProgram(program);
-            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)mvp);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            m_app->draw(width, height);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
@@ -150,6 +90,5 @@ class ContextManager
         glfwDestroyWindow(window);
 
         glfwTerminate();
-        exit(EXIT_SUCCESS);
     }
 };
--- src/glfw_test.cpp
+++ src/glfw_test.cpp
@@ -1,7 +1,19 @@
 #include "glfw.hpp"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using ::testing::AtLeast;
+
+struct TestApp: public Application{
+    MOCK_METHOD(void, key_callback, (GLFWwindow *window, int key, int scancode, int action, int mods), (override));
+    MOCK_METHOD(void, setup, (), (override));
+    MOCK_METHOD(void, draw, (int width, int height), (override));
+};
 
 TEST(GLFWTest, simpleProgram)
-{
-    EXPECT_EXIT(ContextManager a{}, testing::ExitedWithCode(EXIT_SUCCESS), "");
+{ 
+    TestApp myApp;
+    EXPECT_CALL(myApp, setup()).Times(1);
+    EXPECT_CALL(myApp, draw(testing::_, testing::_)).Times(AtLeast(1));
+    ContextManager a{&myApp};
 }
