169fc8f6860fcba0610ff517628d2f3e0d7b1d2f
David Eilers <david.eilers@s2017.tu-chemnitz.de>
2022-01-31 15:18:52 +0100

    Now copies assets folder to build-dir

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#Copies files/directorys if the copy(TO) doesn't exist or if the source(FROM) is newer
 FROM=$1
 TO=$2
 echo "Trying: cp $FROM $TO"
-if [ ! -f "$TO" ]; then
-    a=$(cp $FROM $TO)
+if [ ! -f "$TO" ] && [ ! -d "$TO" ]; then #If destination doesn't exist
+    a=$(cp -r $FROM $TO)
+    exit $a
+elif ["$FROM" -nt "$TO"]; then #If source is newer than destination
+    $(rm -r $TO)
+    a=$(cp -r $FROM $TO)
     exit $a
 fi
 exit 0
