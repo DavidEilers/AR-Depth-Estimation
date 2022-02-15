6e327ab59b650acd0b789a7d889d2c3ca4620c70
David Eilers <david.eilers@s2017.tu-chemnitz.de>
2022-02-15 17:25:54 +0100

    changed to a simpler draw method

--- src/application_vr.hpp
+++ src/application_vr.hpp
@@ -12,113 +12,6 @@ extern "C"{
 
 namespace arDepthEstimation{
 
-class VRApplication : public Application{
-
-     struct Vertex
-    {
-        float x, y;
-    };
-
-    //A screen quad in OpenGL [-1, 1] x [-1, 1] screen-space
-    Vertex vertices[6] = 
-    {// top-left     lower left    top right
-        {-1, -1},    {-1, 1},      {1, -1},
-    //  lower left   lower right   upper right
-        {-1, 1},     {1, 1},       {1, -1}
-    };
-
-    arDepthEstimation::Vr* vr;
-    GLuint vertex_buffer, vertex_array_object;
-    Shader* myShader;
-    GLuint offset_loc;
-
-    
-    public:
-
-    void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods){
-        logger_info << "Key was pressed!";
-    }
-
-    void setup(){
-        glGenVertexArrays(1, &vertex_array_object);
-        glBindVertexArray(vertex_array_object);
-        glGenBuffers(1, &vertex_buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void *)0);
-
-        glClearColor(0.5f, 0.5f, 0.5, 1.0);
-
-        std::string shader_dir("assets\\shader\\");
-        std::string vertex_shader_path(shader_dir + "screen_quad.vert.glsl");
-        std::string fragment_shader_path(shader_dir + "screen_quad.frag.glsl");
-
-        myShader =  new Shader{vertex_shader_path, fragment_shader_path};
-
-        offset_loc = glGetUniformLocation(myShader->m_program_id,"offset");
-        vr = new arDepthEstimation::Vr{};
-    }
-
-    void draw(int width, int height){
-
-        vr->start_frame();
-        
-        logger_info << "update texture start";
-        vr->update_texture();        
-        logger_info << "update texture finished";
-
-        glUseProgram(myShader->m_program_id);
-        
-        vr->texture->bind();
-        glEnable(GL_MULTISAMPLE);
-
-        
-        logger_info << "left eye rendering start";
-       
-        vr->bind_left_eye();
-            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-            glUniform1f(offset_loc,0.0);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-            vr->blit_frame_left();
-        
-        logger_info << "left eye rendering finished";
-
-
-        logger_info << "right eye rendering start";
-
-        vr->bind_right_eye();
-            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-            glUniform1f(offset_loc,0.5);
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-            vr->blit_frame_right();
-
-        logger_info << "right eye rendering finished";
-
-        vr->bind_window();
-        
-        vr->texture->unbind();
-
-        logger_info << "vr->submit frames start";
-        vr->submit_frames();
-        logger_info << "vr->submit frames finished";
-        glFinish();
-
-    }
-
-
-    GLuint get_left_framebuffer_texture_id(){
-        return vr->get_left_framebuffer_texture_id();
-    }
-
-    ~VRApplication(){
-        delete vr;
-        delete myShader;
-    }
-
-};
 
 class MainApplication : public Application{
 
@@ -138,7 +31,7 @@ class MainApplication : public Application{
     GLuint vertex_buffer, vertex_array_object;
     Shader* myShader;
     GLuint offset_loc;
-    VRApplication *  vr;
+    arDepthEstimation::Vr* vr;
 
 
     public:
@@ -176,34 +69,53 @@ class MainApplication : public Application{
 
         offset_loc = glGetUniformLocation(myShader->m_program_id,"offset");
 
-        vr = new VRApplication{};
-        vr->setup();
+        vr = new Vr{};
 
     }
 
     void draw(int width, int height){
-        
+        draw_vr();
+        draw_window(width, height);
+        glFlush();
+        glFinish();
+    }
 
-        logger_info << "draw frame started";
-
-        vr->draw(width,height);
-
-        
-        logger_info << "window rendering start";
-
+    void inline draw_window(int width, int height){
+        vr->bind_window();
         glBindTexture(GL_TEXTURE_2D,vr->get_left_framebuffer_texture_id() );
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glViewport(0, 0, width, height);
             glDrawArrays(GL_TRIANGLES, 0, 6);
 
+    }
+
+    void inline draw_vr(){ 
+        vr->start_frame();
+        vr->update_texture();
+        glUseProgram(myShader->m_program_id);
         
-        logger_info << "window rendering finished";
+        vr->texture->bind();
+        glEnable(GL_MULTISAMPLE);
+       
+        vr->bind_left_eye();
+            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+            glUniform1f(offset_loc,0.0);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            vr->blit_frame_left();
+
+
+        vr->bind_right_eye();
+            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+            glUniform1f(offset_loc,0.5);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            vr->blit_frame_right();
         
         glDisable(GL_MULTISAMPLE);
         
-        glFlush();glFinish();
+        vr->texture->unbind();
+        vr->submit_frames();
+        glFinish();
 
-        logger_info << "draw frame finished";
     }
 
     ~MainApplication(){
